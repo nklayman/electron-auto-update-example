824d7a87e300c2c4638446ca17a8e8886284bfd2
Noah Klayman <noahklayman@gmail.com>
2019-10-10 19:44:32 -0700

    add message that user is on old version of app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is the old version of the app. You should get an update notification shortly."/>
   </div>
 </template>
 
